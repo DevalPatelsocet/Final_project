6699d14083f44ccae67c6be3982f9ad9dd558dfe
DevalPatelsocet <62114979+DevalPatelsocet@users.noreply.github.com>
2021-01-27 22:06:50 -0500

    added one more Query

--- FinalAPIProject/src/main/java/com/finalproject/rest/EmployeeController.java
+++ FinalAPIProject/src/main/java/com/finalproject/rest/EmployeeController.java
@@ -66,4 +66,58 @@ public class EmployeeController {
 
 		return Response.status(status).entity(mainobj.toString()).build();
 	}
+	@POST
+	@Path("/create")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createEmployee(Employee employee) {
+		Sqlconnector connection = new Sqlconnector();
+
+		con = connection.getConnection();
+
+		int status = 200;
+		try {
+
+			String query = "INSERT INTO employee(EMP_ID,END_DATE,FIRST_NAME,LAST_NAME,START_DATE,TITLE,ASSIGNED_BRANCH_ID,DEPT_ID,SUPERIOR_EMP_ID)VALUES(?,?,?,?,?,?,?,?,?);";
+
+			preparedStatement = con.prepareStatement(query);
+
+			preparedStatement.setInt(1, employee.getEmployeeId());
+			preparedStatement.setString(2, employee.getEnddate());
+			preparedStatement.setString(3, employee.getFirstName());
+			preparedStatement.setString(4, employee.getLastName());
+			preparedStatement.setString(5, employee.getStartDate());
+			preparedStatement.setString(6, employee.getTitle());
+			preparedStatement.setInt(7, employee.getAssignBranchId());
+			preparedStatement.setInt(8, employee.getDepartmentId());
+			preparedStatement.setInt(9, employee.getSuperiorEmployeeId());
+
+			int rowCount = preparedStatement.executeUpdate();
+
+			if (rowCount > 0) {
+				System.out.println("Record Inserted Successfully");
+
+				mainobj.accumulate("status", 200);
+				mainobj.accumulate("message", "Successfully Updated");
+			} else {
+				status = 500;
+				mainobj.accumulate("status", 500);
+				mainobj.accumulate("message", "Something Wrong");
+			}
+
+		} catch (SQLException e) {
+			status = 500;
+			mainobj.accumulate("status", 500);
+			mainobj.accumulate("message", e.getMessage());
+		} finally {
+			try {
+				con.close();
+				preparedStatement.close();
+			} catch (SQLException e) {
+				System.out.println("Finally Block SQL Exception : " + e.getMessage());
+			}
+		}
+		return Response.status(status).entity(mainobj.toString()).build();
+	}
+
 }
